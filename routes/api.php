3ce7ef7790a036215df9dfeab18cd37f20516596
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2025-08-18 21:24:25 +0500

    Refactor: Update namespace in API routes.

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use PackageAIQueryOptimizer\Http\Controllers\OptimizerController;
+use AIQueryOptimizer\Http\Controllers\OptimizerController;
 
 Route::post('/ai-query-optimizer/analyze', [OptimizerController::class, 'analyze']);
